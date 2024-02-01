8c07195033de95d819718d732a52108fb3e2241f
jahids-web <jahiduddin922@gmail.com>
2024-02-01 23:38:53 +0600

    Add New Filed

--- Dapper-Demo/Dapper-Demo/Entities/Users.cs
+++ Dapper-Demo/Dapper-Demo/Entities/Users.cs
@@ -1,10 +1,14 @@
-﻿namespace Dapper_Demo.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dapper_Demo.Entities
 {
     public class Users
     {
+        [Key]
         public long Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
+        public string? Password { get; set; }
 
     }
 }
