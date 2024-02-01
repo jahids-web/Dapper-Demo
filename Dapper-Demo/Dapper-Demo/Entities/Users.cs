71b42ddb32215172f50e8f014799d98446510f9a
jahids-web <jahiduddin922@gmail.com>
2024-02-01 23:42:14 +0600

    Add Required Fields

--- Dapper-Demo/Dapper-Demo/Entities/Users.cs
+++ Dapper-Demo/Dapper-Demo/Entities/Users.cs
@@ -6,8 +6,14 @@ namespace Dapper_Demo.Entities
     {
         [Key]
         public long Id { get; set; }
+
+        [Required]
         public string? Name { get; set; }
+
+        [Required]
         public string? Email { get; set; }
+
+        [Required]
         public string? Password { get; set; }
 
     }
